78290be686fc0016bdda301d5709286a5d0555b9
Pyotr <pyotrk@inbox.lv>
2025-08-13 22:59:45 +0300

    Sync MovementHighlighter from origin/my-yesterday

--- BearerOfTheScroll/Assets/Scripts/GameProcess.cs
+++ BearerOfTheScroll/Assets/Scripts/GameProcess.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,7 +6,6 @@ public class GameProcess : MonoBehaviour
 {
     [Header("Gameplay")] 
     [SerializeField] private GameObject playerPrefab;
-    [SerializeField] private MovementHighlighter movementHighlighter;
 
     [Header("Events")]
     [SerializeField] private GameEvent onGameStartedEvent;
@@ -24,14 +22,10 @@ public class GameProcess : MonoBehaviour
         PlayerController player = playerGO.GetComponent<PlayerController>();
 
         CameraFollow cameraFollow = Camera.main.GetComponent<CameraFollow>();
-
         if (cameraFollow != null)
         {
             cameraFollow.SetTarget(player.transform);
         }
-
-        List<Vector3> availablePositions = FindAvailableHexesNear(spawnPosition); 
-        movementHighlighter.ShowAvailableTiles(spawnPosition, availablePositions);
 
         if (onGameStartedEvent !=null)
         {
@@ -43,36 +37,6 @@ public class GameProcess : MonoBehaviour
             onPlayerSpawnedEvent.Raise(player);
             //Debug.Log("Event PlayerSpawnEvent Goooo");
         }
-
             
-    }
-
-    private List<Vector3> FindAvailableHexesNear(Vector3 center)
-    {
-        List<Vector3> result = new List<Vector3>();
-
-        foreach (HexTile hex in FindObjectsOfType<HexTile>())
-        {
-            float distance = Vector3.Distance(center, hex.transform.position);
-            if (distance <= 2.0f)
-            {
-                result.Add(hex.transform.position);
-            }
-        }
-
-        return result;
-    }
-
-    public void HighlightAvailableTiles(Vector3 playerPosition)
-    {
-        List<Vector3> availablePositions = FindAvailableHexesNear(playerPosition);
-
-        movementHighlighter.ShowAvailableTiles(playerPosition, availablePositions);
-
-        MovementLimiter limiter = FindObjectOfType<MovementLimiter>();
-        if (limiter != null)
-        {
-            limiter.SetAvailableHexes(availablePositions);
-        }
     }
 }
