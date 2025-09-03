6285217b2fea5772856c95e63accb7339f64d98a
Pyotr <pyotrk@inbox.lv>
2025-09-03 21:27:40 +0300

    Added hearts on HUD

--- BearerOfTheScroll/Assets/Scripts/Buttons/HUDLivesHearts.cs
+++ BearerOfTheScroll/Assets/Scripts/Buttons/HUDLivesHearts.cs
@@ -1,36 +1,88 @@
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HUDLivesHearts : MonoBehaviour
 {
-    [SerializeField] private FallManager fallManager;
+    [SerializeField] private FallManager fallManager;   
+    [SerializeField] private GameObject[] hearts;       
 
-    [Header("Hearts")]
-    [SerializeField] private GameObject[] hearts; // Heart_1, Heart_2, Heart_3
+    private bool subscribed;
+
+    private void Awake()
+    {       
+        EnsureFallManagerRef();
+    }
 
     private void OnEnable()
     {
-        if (fallManager != null)
-            fallManager.OnLivesChanged += UpdateHearts;
+        EnsureFallManagerRef();
+        TrySubscribe();
+        ForceRefresh();
     }
 
     private void OnDisable()
     {
-        if (fallManager != null)
+        TryUnsubscribe();
+    }
+
+    private void EnsureFallManagerRef()
+    {
+        
+        if (fallManager == null || !fallManager.gameObject.scene.IsValid())
+        {
+            fallManager = FindObjectOfType<FallManager>(); 
+        }
+    }
+
+    private void TrySubscribe()
+    {
+        if (fallManager != null && !subscribed)
+        {
+            fallManager.OnLivesChanged += UpdateHearts;
+            subscribed = true;
+        }
+    }
+
+    private void TryUnsubscribe()
+    {
+        if (fallManager != null && subscribed)
+        {
             fallManager.OnLivesChanged -= UpdateHearts;
+            subscribed = false;
+        }
     }
 
     private void Start()
     {
+        ForceRefresh();
+    }
+
+    private void ForceRefresh()
+    {
         if (fallManager != null)
             UpdateHearts(fallManager.Lives);
+        else
+            ShowAllHearts(); 
+    }
+
+    private void ShowAllHearts()
+    {
+        for (int i = 0; i < hearts.Length; i++)
+            if (hearts[i]) hearts[i].SetActive(true);
     }
 
     private void UpdateHearts(int currentLives)
     {
         for (int i = 0; i < hearts.Length; i++)
         {
-            if (hearts[i] != null)
-                hearts[i].SetActive(i < currentLives);
+            if (!hearts[i]) continue;
+            var img = hearts[i].GetComponent<Image>();
+            if (img != null)
+            {
+                var c = img.color;
+                c.a = (i < currentLives) ? 1f : 0.3f; 
+                img.color = c;
+            }
         }
     }
 }
